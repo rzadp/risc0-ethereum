a5540a688ed631293373d6f8f43e8548228b6c22
Wolfgang Welz <welzwo@gmail.com>
2024-09-13 12:24:53 +0200

    fix license check

--- license-check.py
+++ license-check.py
@@ -34,7 +34,7 @@ SKIP_DIRS = [
     str(Path.cwd()) + "/contracts/src/test/utils/Strings2.sol",
     str(Path.cwd()) + "/examples/erc20-counter/contracts/ERC20.sol",
     # ignore vendored dependencies
-    str(Path.cwd()) + "/steel/src/contracts/vendor",
+    str(Path.cwd()) + "/steel/src/vendor",
 ]
 
 def check_header(expected_year, lines_actual):
